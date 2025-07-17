90f2974835a35639cc5d3070118cb48fff787c00
652367013 <652367013@qq.com>
2025-07-17 23:03:21 +0800

    完成了玩家血量归零时,触发gameover场景

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,9 +7,11 @@ extends Node2D
 @onready var player: CharacterBody2D = $Player
 @onready var enemy_spawner: Node2D = $EnemySpawner
 @onready var hud: Control = $UI/Hud
+@onready var ui: CanvasLayer = $UI
+
+var game_over:PackedScene=preload("res://scenes/game_over.tscn")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	death_zone.area_entered.connect(_on_death_zone_area_entered)
 	player.damage.connect(_on_player_damage)
@@ -24,13 +26,17 @@ func _on_player_damage() -> void:
 	player_health-=1
 	hud.set_health_label(player_health)
 	if player_health==0:
-		print('YOU LOSE')
 		player.queue_free()
+		var game_over_instance=game_over.instantiate()
+		ui.add_child(game_over_instance)
+		game_over_instance.set_score(player_score)
+		
+		
 
 func _on_enemy_spawner_enemy_emit(enemy_instance: Variant) -> void:
 	add_child(enemy_instance)
 	enemy_instance.died.connect(_on_enemy_died)
-	
+#敌人死亡时,得分增加
 func _on_enemy_died():
 	player_score+=100
 	hud.set_score_label(player_score)
---
+++ scripts/game_over.gd
@@ -0,0 +1,20 @@
+extends Control
+@onready var button: Button = $Panel/Button
+@onready var score: Label = $Panel/Score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button.button_down.connect(_on_button_button_down)
+	
+
+
+
+#当按钮下后,重新加载游戏场景
+func _on_button_button_down() -> void:
+	#TODO:完善功能
+	#print("按钮已按下")
+	get_tree().reload_current_scene()
+
+func set_score(new_score):
+	score.text="SCORE: "+str(new_score)
